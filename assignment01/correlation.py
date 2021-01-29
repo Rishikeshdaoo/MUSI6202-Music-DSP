66c19661590152b3bdcf48145b5cca74a7dfc6f3
rishikeshdaoo <rishikesh.daoo@gmail.com>
2021-01-28 22:56:41 -0500

    Assignment 1 - changes to padding and lag calculation

--- assignment01/correlation.py
+++ assignment01/correlation.py
@@ -7,7 +7,7 @@ def crossCorr(x, y):
 
     corr_length = len(x) + len(y) - 1
     corr = np.zeros(corr_length)
-    padded_x = np.hstack((np.zeros((len(y))), x, np.zeros(len(y) - 1)))
+    padded_x = np.hstack((np.zeros((len(y)-1)), x, np.zeros(len(y) - 1)))
 
     for n in range(corr_length):
         corr[n] = np.dot(padded_x[n:n+len(y)], y)
@@ -31,13 +31,15 @@ def findSnarePosition(snareFilename, drumloopFilename):
     snare = loadSoundFile(snareFilename)
     drums = loadSoundFile(drumloopFilename)
 
-    corr = crossCorr(snare, drums)
+    corr = crossCorr(drums, snare)
 
     # Assumed a threshold of 0.8 would be a safe value after looking at the plot from Q.1
     corr_thresholded = np.where(corr >0.8, corr, 0)
 
     pos = argrelextrema(corr_thresholded, np.greater)
-    pos = np.ndarray.tolist(pos[0])
+
+    # Subtracting the padding added in the beginning
+    pos = np.ndarray.tolist(pos[0] - (len(snare) - 1))
 
     return pos
 
@@ -48,6 +50,8 @@ if __name__ == "__main__":
 
     corr = crossCorr(loop, snare)
     lag = range(-len(snare)+1, len(loop))
+
+    pos = findSnarePosition("./snare.wav", "./drum_loop.wav")
 
     plt.plot(lag, corr)
     plt.xlabel("Lag (In samples)")
